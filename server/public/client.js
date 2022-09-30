7bb0c1af16c28e6f698a8c38fb0a938daf4c5631
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-30 18:13:08 -0500

    added code to equalbtn

--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,5 @@
 console.log('in client.js');
+let regexp = /^(\b0\.0*)*?[1-9]\d*(\.\d+)?[\+\*\/\-](\b0\.0*)*?[1-9]\d*(\.\d+)?$/;
 
 $(document).ready(onReady);
 
@@ -7,17 +8,27 @@ let mathInputString='';
 function onReady(){
     console.log('onReady');
 //eventhandlers
+    $('#calcForm').on('submit',onEqualBtn);
     $('.fourbyfour').on('click',on4by4Click);
-
+    $('#clearBtn').on('click',onClearBtn);
+    
 }
 
 function on4by4Click(evt){
     evt.preventDefault();
-    console.log('on4by4Click');
-
-   mathInputString+=$(this).text();
-   render();
-    //$(this).text();
+    //console.log('on4by4Click');
+    mathInputString+=$(this).text();
+    render();
+}
+function onClearBtn(evt){
+    evt.preventDefault();
+    console.log('in onClearBtn');
+    mathInputString='';
+    $('#mathInput').val('');
+}
+function onEqualBtn(evt){
+    evt.preventDefault();
+    console.log('in onEqualBtn');
 }
 function render(){
     $('#mathInput').val(mathInputString);
