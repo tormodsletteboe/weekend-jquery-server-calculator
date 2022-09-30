cfe6973f8196446ae6267445ce34b0886893fd26
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-29 22:58:22 -0500

    fill mathInput with info

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,24 @@
 console.log('in client.js');
+
+$(document).ready(onReady);
+
+let mathInputString='';
+
+function onReady(){
+    console.log('onReady');
+//eventhandlers
+    $('.fourbyfour').on('click',on4by4Click);
+
+}
+
+function on4by4Click(evt){
+    evt.preventDefault();
+    console.log('on4by4Click');
+
+   mathInputString+=$(this).text();
+   render();
+    //$(this).text();
+}
+function render(){
+    $('#mathInput').val(mathInputString);
+}
