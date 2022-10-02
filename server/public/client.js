0c71a29b974a6589e5b8df9f2e513f7b8941955f
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-02 11:08:22 -0500

    base mode with stretch UI works

--- server/public/client.js
+++ server/public/client.js
@@ -1,73 +1,88 @@
 
-//console.log('in client.js');
+//use this regular expression to test input from the calculator before its sent to server
+//this regex is not complete, as it will not pass numbers such as .0000001 or 0.0
 let regexp = /^(\b0\.0*)*?[1-9]\d*(\.\d+)?[\+\*\/\-](\b0\.0*)*?[1-9]\d*(\.\d+)?$/;
 
 $(document).ready(onReady);
 
+//the main display of the calculator
 let mathInputString='';
+//fill this with info form the server
 let clientResultsAndEqns=[];
 
+//onReady, registser all event handlers
 function onReady(){
-    
-   
 //eventhandlers
     $('#calcForm').on('submit',onEqualBtn);
     $('.fourbyfour').on('click',on4by4Click);
     $('#clearBtn').on('click',onClearBtn);
     $('#mathInput').on('focusout',updateMathString)
-    //$('#clearHistoryBtn').on('click',onClearHistory);
-    //runthisOnload();
+    //$('#clearHistoryBtn').on('click',onClearHistory); //TODO for stretch goals
+    
 }
-// function updateMathString(){
-//     //console.log('this ran');
-//     mathInputString = $('#mathInput').val();
+//updateMathString
+//used to update mathInputString when user types with keyboard directly into the input element
+function updateMathString(){
+    mathInputString = $('#mathInput').val();
+}
+
+//onClearHistory
+//TODO: for stretch goals
+// function onClearHistory(){
+    
 // }
 
-function onClearHistory(){
-    
-}
+//on4by4Click
+// anytime 1 of the buttons in the 4 by 4 grid is clicked(except the =), append the string that represents the main display
 function on4by4Click(evt){
     evt.preventDefault();
-    //console.log('on4by4Click');
+    //append the text of the button clicked to the variable mathInputString
     mathInputString+=$(this).text();
-    render();
+    //update the DOM since mathInputString has changed
+    renderMathInput();
 }
+//testOfString
+//test the string before its sent to server. its tested against the global regex
 function testOfString(stringToTest){
     
-    console.log(stringToTest);
+    //console.log(stringToTest);
     let doesItfollowThisPattern = regexp.test(stringToTest);
-    console.log(doesItfollowThisPattern);
+    //console.log(doesItfollowThisPattern);
     if(doesItfollowThisPattern){
         return true;
     }
     return false;
 }
+
+//onClearBtn
+//clear the global variale that represents the main display, and empty some html elements
 function onClearBtn(evt){
     evt.preventDefault();
-    //console.log('in onClearBtn');
     mathInputString='';
     $('#mathInput').val('');
-    $('#errorLabel').text('');
-
 }
+
+//onEqualBtn
+//get equation from the dom, test it against regex, create an obj, send obj to server, get results from server,render results to the dom
 function onEqualBtn(evt){
     evt.preventDefault();
+    //grab the input equation from the dom
     mathInputString=$('#mathInput').val();
+
     //test input before anything else is allowed to happen
     let testThisString = mathInputString;
-    console.log('in onEqualBtn what is testStrng',testThisString);
     if(testOfString(testThisString)===false){
-        //$('#errorLabel').text('Please input a number then an operator then another number. Do not input numbers starting with . or use e as exponent')
         alert("Examples:\n8+2\n5.6/4.7\n0.1-1\n3*5.2");
         return;
     }
 
 
-    //console.log('in onEqualBtn');
+    //create and obj to be sent to the server
     let matheqn = {
         matheqnstring: mathInputString
     };
-    //console.log('mathstring',matheqn.matheqnstring);
+
+
     //send the equation to the server
     $.ajax({
         url:'/mathEqns',
@@ -75,7 +90,6 @@ function onEqualBtn(evt){
         data: matheqn
     })
     .then((response)=>{
-        //console.log('in /mathEqns GET',response);
         if(response === 'Created'){
             //console.log('Created worked');
         }
@@ -83,13 +97,14 @@ function onEqualBtn(evt){
     .catch((err)=>{
         console.log('en /mathEqns err',err);
     });
+
+
     //get the result from the server
     $.ajax({
         url:'/mathEqns/Result',
         method: 'GET'
     })
     .then((response)=>{
-        //console.log('response is',response);
         clientResultsAndEqns = response;
         renderResult();
     })
@@ -98,12 +113,21 @@ function onEqualBtn(evt){
     });
     
 }
-function render(){
+//renderMathInput
+//mathInputString has changed, render the changes to the dom
+function renderMathInput(){
     $('#mathInput').val(mathInputString);
 }
+//renderResult
+//the server has sent the results back, render the most recent equation to the result element, and render the history to the ul
 function renderResult(){
+    //render most recent calculation
     $('#resultH2').text(clientResultsAndEqns[clientResultsAndEqns.length-1].result);
+
+    //empty ul first
     $('#eqnsUL').empty();
+    
+    //render the history
     for(let reslts of clientResultsAndEqns){
         $('#eqnsUL').append(`
         <li>${reslts.matheqnstring}</li>
