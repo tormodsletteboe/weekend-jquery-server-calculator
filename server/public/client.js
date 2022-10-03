3f2671920aadb5bae4f15bd7d3ca7631c8162464
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-03 18:15:22 -0500

    philip test

--- server/public/client.js
+++ server/public/client.js
@@ -98,7 +98,8 @@ function onEqualBtn(evt){
     //test input before anything else is allowed to happen
     let testThisString = mathInputString;
     if(testOfString(testThisString)===false){
-        alert("Wrong input!!!\nExamples of good input:\n8+2\n5.6/4.7\n0.1-1\n3*5.2");
+        alert('Philip your awesome!!!');
+        //"Wrong input!!!\nExamples of good input:\n8+2\n5.6/4.7\n0.1-1\n3*5.2"
         return;
     }
 
